4e11493bf854f099dfc280773592fd20e64cc0c1
Bastian Ilso <contact@bastianilso.com>
2020-04-23 13:53:26 +0200

    Update Hospital Prefab and rename

---
+++ Assets/Scripts/HospitalController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HospitalController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator hospitalAnimator;
+
+    [SerializeField]
+    private ParticleSystem particleSystem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayHospitalFull() {
+        particleSystem.Play();
+        hospitalAnimator.SetBool("HospitalFull", true);
+    }
+}
