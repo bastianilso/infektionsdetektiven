bbcc10398653fe36d7877dd9d40edd7130f5e771
Bastian Ilso <contact@bastianilso.com>
2020-05-20 17:33:30 +0200

    MagnifyGlassLevel: Use Billboards instead of 3D
    We were using sphere 3D objects for each person,
    but this proved to be performance intensive.
    Replace with billboard quads instead.

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class billboard : MonoBehaviour
+{
+    public Camera m_Camera;
+ 
+    void Awake() {
+        if (m_Camera == null) {
+            m_Camera = Camera.main;
+        }
+    }
+
+    //Orient the camera after all movement is completed this frame to avoid jittering
+    void LateUpdate()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+    }
+
+}
